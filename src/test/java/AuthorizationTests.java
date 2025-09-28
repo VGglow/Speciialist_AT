4ae12c8708d3082615066d060e723a06cfa8447e
vadim glazkov <vgglow@gmail.com>
2025-09-28 13:29:11 +0500

    add quotes

--- src/test/java/AuthorizationTests.java
+++ src/test/java/AuthorizationTests.java
@@ -8,26 +8,26 @@ public class AuthorizationTests {
     @Test
     void test01_login_success() {
         open("https://www.saucedemo.com/");
-        $("[data-test=username]").setValue("visual_user");
+        $("[data-test='username']").setValue("visual_user");
         sleep(1_000);
-        $("[data-test=password]").setValue("secret_sauce");
+        $("[data-test='password']").setValue("secret_sauce");
         sleep(2_000);
-        $("[data-test=login-button]").click();
-        $("[data-test=primary-header]").shouldHave(text("Swag Labs"));
-        $("[data-test=title]").shouldHave(text("Products"));
+        $("[data-test='login-button']").click();
+        $("[data-test='primary-header']").shouldHave(text("Swag Labs"));
+        $("[data-test='title']").shouldHave(text("Products"));
         sleep(3_000);
     }
 
     @Test
     void test02_login_wrong_password() {
         open("https://www.saucedemo.com/");
-        $("[data-test=username]").setValue("visual_user");
+        $("[data-test='username']").setValue("visual_user");
         sleep(1_000);
-        $("[data-test=password]").setValue("wrong_pwd");
+        $("[data-test='password']").setValue("wrong_pwd");
         sleep(2_000);
-        $("[data-test=login-button]").click();
+        $("[data-test='login-button']").click();
         $(".login_container").shouldHave(text("Swag Labs"));
-        $("[data-test=error]").shouldHave(text("Epic sadface: Username and password do not match any user in this service"));
+        $("[data-test='error']").shouldHave(text("Epic sadface: Username and password do not match any user in this service"));
         sleep(5_000);
     }
 }
